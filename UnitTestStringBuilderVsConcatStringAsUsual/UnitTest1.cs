40b74ca2db0d4f0b0de98877ad972586b8f867e2
Daniel Carrilho <djtcarrilho@hotmail.com>
2016-12-29 15:52:38 +0000

    code base

---
+++ UnitTestStringBuilderVsConcatStringAsUsual/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestStringBuilderVsConcatStringAsUsual
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConcatStringAsUsual()
+        {
+            //Arranje
+          
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestStringBuilder()
+        {
+            //Arranje
+
+            //Act
+
+            //Assert
+        }
+
+
+    }
+}
