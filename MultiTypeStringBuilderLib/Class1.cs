d250c3a0b67d5d901aa3f6d30cf64547a881e029
Daniel Carrilho <djtcarrilho@hotmail.com>
2017-01-05 17:56:24 +0000

    code base, a bunch of methods to test and a bunch of tests

--- MultiTypeStringBuilderLib/Class1.cs
+++ MultiTypeStringBuilderLib/Class1.cs
@@ -8,5 +8,152 @@ namespace MultiTypeStringBuilderLib
 {
     public class MultiTypeStringBuilder
     {
+        public string ShortStringConcatUsingStringBuilder(String inputString, string stringToAppend)
+        {
+            StringBuilder sb = new StringBuilder(inputString);
+            sb.Append(stringToAppend);
+            String result = sb.ToString();
+            return result;
+        }
+
+        public string ShortStringConcatUsingPlusOperator(String inputString, string stringToAppend)
+        {
+            inputString  += stringToAppend;
+            return inputString;
+        }
+
+        public string ShortStringConcatUsingStringFormat(String inputString, string stringToAppend)
+        {
+            string result = String.Format("{0}{1}", inputString, stringToAppend);
+            return result;
+        }
+
+        public string ShortStringConcatUsingStringConcat(String inputString, string stringToAppend)
+        {
+            String result = String.Concat(inputString, stringToAppend);
+            return result;
+        }
+
+        public string ShortStringConcatUsingStringJoin(String inputString, string stringToAppend)
+        {
+            //does not enter on the validation of the string concatenation performance
+            List<String> strings = new List<string>();
+            strings.Add(inputString);
+            strings.Add(stringToAppend);
+            string stringSeparator = "";
+
+            String result = String.Join(stringSeparator , strings);
+            return result;
+        }
+
+        //fastest
+        public string LongStringConcatUsingStringBuilder()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.Append("a");
+            }
+            return sb.ToString();
+        }
+
+        //slowest
+        public string LongStringConcatUsingPlusOperator()
+        {
+            String result = "";
+            for (int i = 0; i < 10000; i++)
+            {
+                result += "a";
+            }
+            return result.ToString();
+        }
+
+        public string LongStringConcatUsingStringFormat()
+        {
+            String result = "%s";
+            for (int i = 0; i < 10000; i++)
+            {
+                result = String.Format("{}", "a");
+            }
+            return result.ToString();
+        }
+
+        public string StringConcatTwoLongStringsUsingPlusOperator()
+        {
+            string s1 = LongStringConcatUsingStringBuilder();
+            string s2 = LongStringConcatUsingStringBuilder();
+
+            string result = s1 + s2;
+
+            return result;
+        }
+
+        public string StringBuilderConcatTwoLongStrings()
+        {
+            StringBuilder sb1 = new StringBuilder(LongStringConcatUsingStringBuilder());
+            StringBuilder sb2 = new StringBuilder(LongStringConcatUsingStringBuilder());
+            sb1.Append(sb2);
+            return sb1.ToString();
+        }
+
+        public void CollectDataFromLongRunOffBothConcatStringUsingStringBuilderAndConcatStringUsingPlusOperator(int howManyRuns)
+        {
+            for (int i = 0; i < howManyRuns; i++)
+            {
+                LongStringConcatUsingStringBuilder();
+                LongStringConcatUsingPlusOperator();
+
+            }
+        }
+
+        public void CollectDataFromLongRunOffBothStringBuilderAndConcatStringAsUsualCustomTiming(int howManyRuns)
+        {
+            double[] timingsStringBuilderConcat = new double[howManyRuns];
+            double[] timingsConcatAsUsual = new double[howManyRuns];
+            DateTime before, after;
+
+            for (int i = 0; i < howManyRuns; i++)
+            {
+                before = DateTime.Now;                
+                LongStringConcatUsingStringBuilder();
+                after = DateTime.Now;
+                timingsStringBuilderConcat[i] = (after - before).TotalMilliseconds;
+
+                before = DateTime.Now;
+                LongStringConcatUsingPlusOperator();
+                after = DateTime.Now;
+                timingsConcatAsUsual[i] = (after - before).TotalMilliseconds;
+            }
+        }
+
+        public void LongStringConcatUsingStringBuilderManyTimes(int howManyRuns, int howManyConcats)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int currentRunValue = 0; currentRunValue < howManyRuns; currentRunValue++)
+            {
+                for (int appendRun = 0; appendRun < howManyConcats; appendRun++)
+                {
+                    sb.Append(howManyConcats.ToString());
+                }
+
+                sb.Clear();
+            }
+        }
+
+        public void LongStringConcatUsingPlusOperatorManyTimes(int howManyRuns, int howManyConcats)
+        {
+            String result = "";
+
+            for (int currentRunValue = 0; currentRunValue < howManyRuns; currentRunValue++)
+            {
+                for (int concatRun = 0; concatRun < howManyConcats; concatRun++)
+                {
+                    result += concatRun.ToString();
+                }
+
+                result = "";
+            }
+        }
     }
 }
--- UnitTestStringBuilderVsConcatStringAsUsual/UnitTest1.cs
+++ UnitTestStringBuilderVsConcatStringAsUsual/UnitTest1.cs
@@ -6,26 +6,131 @@ namespace UnitTestStringBuilderVsConcatStringAsUsual
     [TestClass]
     public class UnitTest1
     {
+        private TestContext myTestContext;
+
+        public TestContext TestContext
+        {
+            get { return myTestContext; }
+            set { myTestContext = value; }
+        }
+        
         [TestMethod]
-        public void TestConcatStringAsUsual()
+        public void TestShortStringConcatUsingPlusOperator()
         {
             //Arranje
             MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
             //Act
+            string result = multiTypeStringBuilder.ShortStringConcatUsingPlusOperator("Daniel", " Carrilho");
+            //Assert
+            Assert.AreEqual(result, "Daniel Carrilho");
+        }
+
+        [TestMethod]
+        public void TestShortStringConcatUsingStringBuilder()
+        {
+            //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
+            //Act
+            string result = multiTypeStringBuilder.ShortStringConcatUsingStringBuilder("Daniel", " Carrilho");
+            //Assert
+            Assert.AreEqual(result, "Daniel Carrilho");
+        }
+
+        [TestMethod]
+        public void TestShortStringConcatUsingStringConcat()
+        {
+            //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
+            //Act
+            string result = multiTypeStringBuilder.ShortStringConcatUsingStringConcat("Daniel", " Carrilho");
+            //Assert
+            Assert.AreEqual(result, "Daniel Carrilho");
+        }
+
+        [TestMethod]
+        public void TestShortStringConcatUsingStringFormat()
+        {
+            //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
+            //Act
+            string result = multiTypeStringBuilder.ShortStringConcatUsingStringFormat("Daniel", " Carrilho");
+            //Assert
+            Assert.AreEqual(result, "Daniel Carrilho");
+        }
+
+        [TestMethod]
+        public void TestShortStringConcatUsingStringJoin()
+        {
+            //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
+            //Act
+            string result = multiTypeStringBuilder.ShortStringConcatUsingStringJoin("Daniel", " Carrilho");
+            //Assert
+            Assert.AreEqual(result, "Daniel Carrilho");
+        }
+        
+        [TestMethod]
+        [DeploymentItem(@"Data\TestData.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"|DataDirectory|\TestData.xml", "Row", DataAccessMethod.Sequential)]
+        public void TestXMLDataDrivenShortStringConcatUsingPlusOperator()
+        {
+            //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
+            //Act
+            string result = multiTypeStringBuilder.ShortStringConcatUsingPlusOperator((String)TestContext.DataRow["inputString"], (String)TestContext.DataRow["stringToAppend"]);
+            //Assert
+            Assert.AreEqual(result, TestContext.DataRow["Result"]);
+        }
+
+
+        [TestMethod]
+        public void testCollectDataFromLongRunOffBothConcatStringUsingStringBuilderAndConcatStringUsingPlusOperator()
+        {
+            //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
+
+            //Act
+            multiTypeStringBuilder.CollectDataFromLongRunOffBothConcatStringUsingStringBuilderAndConcatStringUsingPlusOperator(1000);
 
             //Assert
         }
 
         [TestMethod]
-        public void TestStringBuilder()
+        public void testcollectDataFromLongRunOffBothStringBuilderAndConcatStringAsUsualCustomTiming()
         {
             //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
 
             //Act
+            multiTypeStringBuilder.CollectDataFromLongRunOffBothStringBuilderAndConcatStringAsUsualCustomTiming(1000);
 
             //Assert
         }
 
+        [TestMethod]
+        public void TestVeryLongStringBuilderConcatManyTimes()
+        {
+            //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
 
+            //Act
+            multiTypeStringBuilder.LongStringConcatUsingPlusOperatorManyTimes(1000, 10000);
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void TestveryLongStringConcatAsUsualManyTimes()
+        {
+            //Arranje
+            MultiTypeStringBuilderLib.MultiTypeStringBuilder multiTypeStringBuilder = new MultiTypeStringBuilderLib.MultiTypeStringBuilder();
+
+            //Act
+            multiTypeStringBuilder.LongStringConcatUsingPlusOperatorManyTimes(1000, 10000);
+
+            //Assert
+
+        }
     }
 }
